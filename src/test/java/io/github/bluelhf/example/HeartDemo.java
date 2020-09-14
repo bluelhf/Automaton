5cba7e994871b918d27125bab1edc6481b7b6fc2
bluelhf <blueloveshisfriends@gmail.com>
2020-09-14 14:27:13 +0300

    Minor comments & Rename Example.java to HeartDemo.java

--- src/test/java/io/github/bluelhf/example/HeartDemo.java
+++ src/test/java/io/github/bluelhf/example/HeartDemo.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.concurrent.locks.LockSupport;
 
-public class Example {
+public class HeartDemo {
     public static void main(String[] args) {
         Automaton automaton;
         try {
@@ -30,19 +30,28 @@ public class Example {
         ArrayList<Point> points = new ArrayList<>();
         ArrayList<Point> temp = new ArrayList<>();
         for(double x = -2; x < 2; x += 0.01) {
+            // Upper half of the heart
             double y1 = Math.sqrt(1 - Math.pow((Math.abs(x) - 1), 2)) + 1;
+
+            // Lower half of the heart
             double y2 = Math.acos(1 - Math.abs(x)) - Math.PI + 1;
 
+
+            // Convert to screen coordinates
             int screenX = (int) (centreX + x*t);
             int screenY1 = (int) (centreY - y1*t);
             int screenY2 = (int) (centreY - y2*t);
+
             points.add(new Point(screenX, screenY1));
+
+            // Add lower half later (and in reverse) so we traverse the points correctly
             temp.add(new Point(screenX, screenY2));
         }
         Collections.reverse(temp);
         points.addAll(temp);
 
         automaton.moveMouse(points.get(0).x, points.get(0).y, -1);
+
         long start = System.currentTimeMillis();
         while (System.currentTimeMillis() - start < 10000) {
             for (Point p : points) {
